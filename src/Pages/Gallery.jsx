c9104241cfc3a4e7b849dfaefc2cf62f01c65b00
Dipesh <techdipesh36@gmail.com>
2025-01-05 14:24:08 +0545

    implemented mansory grid gallery in gallery page

---
+++ src/Pages/Gallery.jsx
@@ -0,0 +1,279 @@
+import React, { useEffect } from 'react'
+import Nav from '../components/Nav'
+import aboutimage from "../assets/images/about.png"
+import historyimage from "../assets/images/history.png"
+import dineimage from "../assets/images/dine.png"
+import dine2image from "../assets/images/dining2.png"
+import bedimage from "../assets/images/bed.png"
+import Footer from '../components/Footer'
+
+import LightGallery from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+// import plugins if you need
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgVideo from 'lightgallery/plugins/video';
+import fjGallery from 'flickr-justified-gallery';
+
+
+const data = [
+    {
+        image: aboutimage,
+        title: "Butcher's Block",
+        description:
+            'Discover the wonderful depths of flavour that only pure wood-fire can forge, in this avant-garde atmospheric live cooking experience.',
+        link: '#',
+    },
+    {
+        image: historyimage,
+        title: 'Smoky Delights',
+        description:
+            'Experience the rich taste of smoky creations, prepared with precision and passion over a live fire.',
+        link: '#',
+    },
+    {
+        image: bedimage,
+        title: 'Flame & Sizzle',
+        description:
+            'Savor the extraordinary flavors of flame-grilled delicacies in a vibrant, immersive dining experience.',
+        link: '#',
+    },
+    {
+        image: dineimage,
+        title: 'Tiffin Room',
+        description:
+            "One of Singapore's oldest North Indian restaurants, serving up the golden age delicacies of the maharajahs since 1892.",
+        link: '#',
+    },
+];
+
+// const gallery = [{
+//     imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/turkis-royal-room-dOqyzB3GPDID1BKE.jpg",
+// },
+// {
+//     imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/lobby-3-YD061kRqy2HpR4b8.jpg",
+// },
+// {
+//     imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/junior-suit-5-mP4nqkRL51CpRkgQ.jpg",
+// },
+// {
+//     imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/junior-suit-AoPvB59kkMhg5Z53.jpg",
+// },
+// ]
+
+const gallery = [
+    {
+        imageUrl: "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=656,h=521,fit=crop/mnl3DyqLpOSqoMLB/generated/generated-mnlv1DRlXRtDpEoD.png",
+    },
+    {
+        imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/turkis-royal-room-dOqyzB3GPDID1BKE.jpg",
+    },
+    {
+        imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/lobby-3-YD061kRqy2HpR4b8.jpg",
+    },
+    {
+        imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/junior-suit-AoPvB59kkMhg5Z53.jpg",
+    },
+  
+    {
+        imageUrl: "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=656,h=718,fit=crop/mnl3DyqLpOSqoMLB/reception-1-mePvl4Oy31iwqyy7.jpg",
+    },
+    {
+        imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/lobby-3-YD061kRqy2HpR4b8.jpg",
+    },
+    {
+        imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/lobby-3-YD061kRqy2HpR4b8.jpg",
+    },
+    {
+        imageUrl: "https://assets.zyrosite.com/mnl3DyqLpOSqoMLB/junior-suit-5-mP4nqkRL51CpRkgQ.jpg",
+    },
+];
+
+const groupImages = (images, columnCount) => {
+    const columns = Array.from({ length: columnCount }, () => []);
+    images.forEach((image, index) => {
+        columns[index % columnCount].push(image);
+    });
+    return columns;
+};
+
+const Gallery = () => {
+    const columnCount = 3;
+    const groupedImages = groupImages(gallery, columnCount);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+    // useEffect(() => {
+    //     fjGallery(document.querySelectorAll('.gallery'), {
+    //         itemSelector: '.gallery__item',
+    //         rowHeight: 180,
+    //         lastRow: 'start',
+    //         gutter: 2,
+    //         rowHeightTolerance: 0.1,
+    //         calculateItemsHeight: false,
+    //     });
+    // }, []);
+
+    const getRandomSpan = () => {
+        const colSpan = Math.floor(Math.random() * 2) + 1; // Random value: 1 or 2
+        const rowSpan = Math.floor(Math.random() * 2) + 1; // Random value: 1 or 2
+        return { col: colSpan, row: rowSpan };
+    };
+
+    return (
+        <>
+            <div className=''>
+                <Nav isTextBlack={true} />
+                <div className=' absolute top-0 left-0 w-full'>
+
+                    {/* hero section */}
+                    <div className="h-[50vh] relative bg-[#E2E0D1]">
+                        <div className='absolute bottom-0 w-full text-center text-black tracking-widest z-20 py-10'>
+                            <h2 className='font-canela text-6xl font-medium tracking-widest mt-8' ><span className='font-canela uppercase'>Explore </span><i className='font-canela font-medium'>our</i> <span className='font-canela uppercase'> World</span></h2>
+                        </div>
+                        <div className='absolute -z-10 top-0 left-0 w-full h-[150%] bg-[#E2E0D1]'>
+                        </div>
+                    </div>
+                    {/* end of hero section */}
+
+                    {/* contents */}
+                    <div className='px-10 mt-10'>
+                        <div className='mb-20'>
+                            <LightGallery
+                                plugins={[lgZoom, lgVideo]}
+                                mode="lg-fade"
+                                pager={false}
+                                thumbnail={true}
+                                galleryId={'nature'}
+                                autoplayFirstVideo={false}
+                                elementClassNames={'gallery'}
+                                mobileSettings={{
+                                    controls: false,
+                                    showCloseIcon: false,
+                                    download: false,
+                                    rotate: false,
+                                }}
+                            >
+                                {gallery.length > 0 && gallery.map((element, index) => (
+                                    <a href={element.imageUrl} key={index} className='gallery-item'>
+                                        <img src={element.imageUrl} alt={element.imageUrl} />
+                                    </a>
+                                ))}
+                            </LightGallery>
+                        </div>
+                    </div>
+
+
+                    {/* <div class="grid grid-cols-2 gap-4 md:grid-cols-4">
+                        <div class="grid gap-4">
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center"
+                                    src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center "
+                                    src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center"
+                                    src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                        </div>
+                        <div class="grid gap-4">
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center"
+                                    src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center"
+                                    src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center "
+                                    src="https://docs.material-tailwind.com/img/team-3.jpg"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                        </div>
+                        <div class="grid gap-4">
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center"
+                                    src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center "
+                                    src="https://docs.material-tailwind.com/img/team-3.jpg"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center"
+                                    src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                        </div>
+                        <div class="grid gap-4">
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center"
+                                    src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    class="h-auto max-w-full rounded-lg object-cover object-center"
+                                    src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+                                    alt="gallery-photo"
+                                />
+                            </div>
+                        </div>
+                    </div> */}
+
+                    {/* <div className="gallery">
+                        {gallery.length > 0 && gallery.map((element, index) => (
+                            <a href="" key={index} className='gallery-item'>
+                                <img src={element.imageUrl} alt="" />
+                            </a>
+                        ))}
+                    </div> */}
+                    {/*end of contents */}
+                    {/* footer */}
+                    <Footer />
+                    {/*end of footer */}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Gallery
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/images/logo.png"
 import logo2 from "../assets/images/logo2.png"
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-const Nav = () => {
+const Nav = ({ isTextBlack }) => {
   const [isDown, setIsDown] = useState(false);
   const triggerHeight = 200; // Height in pixels to toggle isDown
 
@@ -27,8 +27,8 @@ const Nav = () => {
   return (
     <>
       <div className="z-20 relative tracking-wider" >
-        <div className=' text-white'>
-          <div className="flex relative justify-center text-sm items-center py-5 text-white border-b border-gray-400 mx-20">
+        <div className={` ${isTextBlack ? "text-black" : "text-white"}`}>
+          <div className={`flex relative justify-center text-sm items-center py-5 ${isTextBlack ? "text-black border-black" : "text-white border-gray-400"} border-b  mx-20`}>
             <Link to="/">
               <div className='w-32'>
                 <img src={logo} alt="" className='w-full object-contain brightness-[2.5]' />
@@ -37,7 +37,7 @@ const Nav = () => {
             <div className='flex absolute right-0 top-0 bottom-0 gap-8 items-center'>
               <h2 className='cursor-pointer'>Sign In</h2>
               <h2 className='cursor-pointer'>Contact</h2>
-              <a href="" className='px-8 py-2 border font-bold text-xs uppercase border-white cursor-pointer  font-akzidenz'>Book</a>
+              <a href="" className={`px-8 py-2 border font-bold text-xs uppercase ${isTextBlack ? "border-black" : "border-white"} cursor-pointer  font-akzidenz`}>Book</a>
             </div>
           </div>
 
@@ -51,7 +51,7 @@ const Nav = () => {
               <li><a className='cursor-pointer' href="">What's on</a></li>
               <li><a className='cursor-pointer' href="">occasions</a></li>
               <li><a className='cursor-pointer' href="">offers</a></li>
-              <li><a className='cursor-pointer' href="">gallery</a></li>
+              <li><Link className='cursor-pointer' to="/gallery">Gallery</Link></li>
               <li><a className='cursor-pointer' href="">gift card</a></li>
             </ul>
           </div>
@@ -59,7 +59,7 @@ const Nav = () => {
       </div>
 
 
-      <div className={`z-20 w-full tracking-wider ${isDown ? "fixed top-0 left-0 transform translate-y-0 opacity-100 pointer-events-auto" : "transform -translate-y-full opacity-0 pointer-events-none"
+      <div className={`z-50 w-full tracking-wider ${isDown ? "fixed top-0 left-0 transform translate-y-0 opacity-100 pointer-events-auto" : "transform -translate-y-full opacity-0 pointer-events-none"
         } transition-transform duration-300 ease-in-out`} >
         <div className=' text-white'>
           <div className={`flex  items-center text-sm py-2 px-10 bg-white shadow-lg text-black justify-between`}>
@@ -84,7 +84,7 @@ const Nav = () => {
               <li><a className='cursor-pointer' href="">What's on</a></li>
               <li><a className='cursor-pointer' href="">occasions</a></li>
               <li><a className='cursor-pointer' href="">offers</a></li>
-              <li><a className='cursor-pointer' href="">gallery</a></li>
+              <li><Link className='cursor-pointer' to="/gallery">Gallery</Link></li>
               <li><a className='cursor-pointer' href="">gift card</a></li>
             </ul>
             <div className={`flex gap-8 items-center`}>
