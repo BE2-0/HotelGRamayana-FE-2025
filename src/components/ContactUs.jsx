e1d6181efc946765200ecfe6a1a4f3a8494ffe9e
Dipesh <techdipesh36@gmail.com>
2025-02-04 11:01:12 +0545

    working on contact us page email sending

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useReducer, useRef, useState } from 'react'
 import { BsTelephone } from "react-icons/bs";
 import { CiMail } from "react-icons/ci";
 import { CiLocationOn } from "react-icons/ci";
 import contactImage from "../assets/Gallery/DSC07999.jpg";
 import { HiXMark } from "react-icons/hi2";
 import logo from "../assets/images/logo.png"
+import toast from 'react-hot-toast';
+import axios from 'axios';
 const ContactUs = ({ contactUsOpen, handleContactUsClose }) => {
+    const apiKey = import.meta.env["VITE_API_SERVICE_URL"];
+    const recipientEmail = import.meta.env["VITE_RECIPIENT_EMAIL"];
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+    const acceptRef = useRef(null);
+    const validateForm = () => {
+        if(!acceptRef.current.checked)
+        {
+            toast.error("Please accept the policy");
+            return false;
+        }
+        if (name.trim() == "" || email.trim() == "" || phone.trim() == "" || message.trim() == "") {
+            toast.error("Please fill all fields!");
+            return false;
+        }
+        return true;
+    }
+    const handleSubmit = async (e) => {
+        if (!validateForm()) {
+            return;
+        }
+        const data = {
+            to: [recipientEmail??"techdipesh36@gmail.com"],
+            subject: "Contact Message from Hotel G Ramayana",
+            content: `
+            Name : ${name}
+            Email : ${email}
+            Phone : ${phone}
+            Message : ${message}
+            `
+        }
+        try {
+            const response = await axios.post(`${apiKey}api/email/sendEmail`,data)
+            if(response.status==200)
+            {
+                toast.success(response.data.message);
+                setName("");
+                setEmail("");
+                setPhone("");
+                setMessage("");
+                acceptRef.current.checked=false;
+            }
+        } catch (error) {
+            toast.error("something went wrong");
+        }
+    }
     return (
         <>
             {/* {contactUsOpen && ( */}
@@ -48,6 +98,8 @@ const ContactUs = ({ contactUsOpen, handleContactUsClose }) => {
                                     type="text"
                                     className="w-full px-4 h-10 pr-3 py-2 bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md peer"
                                     placeholder=""
+                                    value={name}
+                                    onChange={(e) => { setName(e.target.value) }}
                                 />
                             </div>
                         </div>
@@ -58,6 +110,8 @@ const ContactUs = ({ contactUsOpen, handleContactUsClose }) => {
                                     type="text"
                                     className="w-full px-4 h-10 pr-3 py-2 bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md peer"
                                     placeholder=""
+                                    value={email}
+                                    onChange={(e) => { setEmail(e.target.value) }}
                                 />
                             </div>
                         </div>
@@ -68,6 +122,8 @@ const ContactUs = ({ contactUsOpen, handleContactUsClose }) => {
                                     type="text"
                                     className="w-full px-4 h-10 pr-3 py-2 bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md peer"
                                     placeholder=""
+                                    value={phone}
+                                    onChange={(e) => { setPhone(e.target.value) }}
                                 />
                             </div>
                         </div>
@@ -78,17 +134,19 @@ const ContactUs = ({ contactUsOpen, handleContactUsClose }) => {
                                     type="text"
                                     className="w-full px-4 h-20 pr-3 py-2 bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md peer"
                                     placeholder=""
+                                    value={message}
+                                    onChange={(e) => { setMessage(e.target.value) }}
                                 />
                             </div>
                         </div>
                         <div className='flex gap-2 items-start'>
-                            <input type="checkbox" id='accept' className='mt-1 cursor-pointer' />
+                            <input type="checkbox" ref={acceptRef} id='accept' className='mt-1 cursor-pointer' />
                             <label htmlFor='accept' className=' text-xs tracking-wider text-gray-600 cursor-pointer'>
                                 Please indicate you have read and agree to the privacy policy of Ramayana Hotel. Your details are private and will never be shared with any third parties.
                             </label>
                         </div>
                         <div className='flex justify-center my-4'>
-                            <button className={`px-8 py-3 border font-bold text-sm uppercase border-gray-500 hover:border-gray-950 hover:text-gray-950 duration-300 text-gray-600 cursor-pointer tracking-[0.15rem]  font-akzidenz`}>submit</button>
+                            <button onClick={handleSubmit} className={`px-8 py-3 border font-bold text-sm uppercase border-gray-500 hover:border-gray-950 hover:text-gray-950 duration-300 text-gray-600 cursor-pointer tracking-[0.15rem]  font-akzidenz`}>submit</button>
                         </div>
                         <div>
                             <p className=' text-xs tracking-wider text-gray-600 text-center'>
